5980ce0f57f78f0881dcc12cedfdd5bc605a9e27
Quevin2020 <45124566+Q2021Q@users.noreply.github.com>
2021-11-02 08:26:26 -0600

    Balance General modificado

--- pages/balanceAexcell.php
+++ pages/balanceAexcell.php
@@ -58,179 +58,134 @@ if($_SESSION["logueado"] == TRUE) {
         }
       }
     }
-    /*------------------ ACTIVO CORRIENTE ------------------*/
-    //Calculo de efectivo y equivalentes
-    $efectivoYER = $conexion->query("select c.nombrecuenta, c.codigocuenta, p.idpartida, p.concepto, p.fecha, l.debe, l.haber from catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo inner join partida p on l.idpartida = p.idpartida WHERE substring(c.codigocuenta,1,4)='1101' and p.idanio='".$anioActivo."'");
-    if($efectivoYER){
-      $saldoEYE=0;
-      while($fila=$efectivoYER->fetch_object()){
-        $saldoEYE=$saldoEYE+($fila->debe)-($fila->haber);
-      }
-    }
-    //Calculo de cuentas por cobrar comerciales
-    $cuentaPCC = $conexion->query("select c.nombrecuenta, c.codigocuenta, p.idpartida, p.concepto, p.fecha, l.debe, l.haber from catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo inner join partida p on l.idpartida = p.idpartida WHERE substring(c.codigocuenta,1,4)='1103' and p.idanio='".$anioActivo."'");
-    if($cuentaPCC){
-      $saldoPCC = 0;
-      while($fila=$cuentaPCC->fetch_object()){
-        $saldoPCC=$saldoPCC+($fila->debe)-($fila->haber);
-      }
-    }
-    //Calculo de IVA Credito Fiscal
-    $IVACF = $conexion->query("select c.nombrecuenta, c.codigocuenta, p.idpartida, p.concepto, p.fecha, l.debe, l.haber from catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo inner join partida p on l.idpartida = p.idpartida WHERE substring(c.codigocuenta,1,4)='1109' and p.idanio='".$anioActivo."'");
-    if($IVACF){
-      $saldoIVACF = 0;
-      while($fila=$IVACF->fetch_object()){
-        $saldoIVACF=$saldoIVACF+($fila->debe)-($fila->haber);
-      }
-    }
-    /*ACTIVO NO CORRIENTE */
 
-    /*PASIVO CORRIENTE*/
-    //Calculo de Cuentas por pagar comerciales
-    $cuentasPP = $conexion->query("select c.nombrecuenta, c.codigocuenta, p.idpartida, p.concepto, p.fecha, l.debe, l.haber from catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo inner join partida p on l.idpartida = p.idpartida WHERE substring(c.codigocuenta,1,4)='2101' and p.idanio='".$anioActivo."'");
-    if($cuentasPP){
-      $saldoCPP = 0;
-      while($fila=$cuentasPP->fetch_object()){
-        $saldoCPP=$saldoCPP+($fila->haber)-($fila->debe);
-      }
-    }
-    //Calculo de IVA debito fiscal
-    $IVADF = $conexion->query("select c.nombrecuenta, c.codigocuenta, p.idpartida, p.concepto, p.fecha, l.debe, l.haber from catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo inner join partida p on l.idpartida = p.idpartida WHERE substring(c.codigocuenta,1,4)='2108' and p.idanio='".$anioActivo."'");
-    if($IVADF){
-      $saldoIVADF = 0;
-      while($fila=$IVADF->fetch_object()){
-        $saldoIVADF=$saldoIVADF+($fila->haber)-($fila->debe);
-      }
-    }
+    //Calculo de saldos en cuentas de activos, pasivos y patrimonio
 
-    
-    
+    $arregloSeccionPrincipal[] = array(1,"ACTIVOS");
+    $arregloSeccionPrincipal[] = array(2,"PASIVOS");
+    $arregloSeccionPrincipal[] = array(3,"PATRIMONIO");
+
+    for($i=1;$i<4;$i++){
+
+      //obtención de cuentas de nivel 2, grupos financieros
+      $consulta ="SELECT codigocuenta, nombrecuenta FROM catalogo WHERE codigocuenta LIKE '".$i."_' order by codigocuenta asc";
+      $gruposfinan = $conexion->query($consulta);
+      if($gruposfinan){
+        while($grupo = $gruposfinan->fetch_object()){
+          $arregloGrupo[] = array($grupo->codigocuenta,$grupo->nombrecuenta);
+        }
+      }
+
+      $consulta = "SELECT codigocuenta, nombrecuenta FROM catalogo WHERE codigocuenta LIKE '".$i."___' order by codigocuenta asc";
+      $cuentas = $conexion->query($consulta);
+      if($cuentas){
+        while($cuenta = $cuentas->fetch_object()){
+          $saldoCuenta = 0;
+          $codigoCuenta = $cuenta->codigocuenta;
+          $nombreCuenta = $cuenta->nombrecuenta;
+
+          //obteniendo subcuentas de la cuenta
+          $consulta = "Select l.debe, l.haber, c.saldo as saldo FROM catalogo c INNER JOIN ldiario l ON c.idcatalogo = l.idcatalogo INNER JOIN partida p ON l.idpartida = p.idpartida WHERE  SUBSTRING(c.codigocuenta, 1,4) = '".$codigoCuenta."' AND p.idanio='".$anioActivo."'";
+          $subcuentas = $conexion->query($consulta);
+          if($subcuentas){
+            while($sub = $subcuentas->fetch_object()){
+              if($sub->saldo == 'DEUDOR'){
+                $saldoCuenta = $saldoCuenta + ($sub->debe) - ($sub->haber);
+              }else{
+                $saldoCuenta = $saldoCuenta - ($sub->debe) + ($sub->haber);
+              }
+            }
+          }
+          $tablaCuentas[] = array($codigoCuenta,$nombreCuenta,$saldoCuenta);
+        }//fin de ciclo de cuentas
+
+      }//fin de saldos de cuentas
+    }//fin de de calculo de saldos por seccion principal
  ?>
 
-<body id="mimin" class="dashboard">
+
+
+
 
 
     <!-- start: Content -->
-     
-        <input type="hidden" name="bandera" id="bandera">
-        <input type="hidden" name="baccion" id="baccion">
+       
                 <center>
                   <h3>Empresa Crisales</h3>
                   <h3>Balance General del periodo</h3>
-                  <h3>Al <?php echo $fechaMaximaDia." de ".$fechaMaximaMes." del ".$anioActivo?></h3>
+                  <h3>Al <?php echo "{$fechaMaximaDia} de {$fechaMaximaMes} del {$anioActivo}" ?></h3>
                 </center>
-                    <table width="70%">
+                    <table width="80%">
                       <thead>
                         <tr>
-                          <th>CONCEPTO</th>
-                          <th>DEBE</th>
+                          <td>CONCEPTO</td>
+                          <th>Debe</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <th>ACTIVO</th>
-                        </tr>
-                        <tr class="bg-light">
-                          <th>ACTIVO CORRIENTE</th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">EFECTIVO Y EQUIVALENTES</td>
-                          <th ><?php echo "$".$saldoEYE ?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">CUENTAS POR COBRAR COMERCIALES</td>
-                          <th<?php echo "$".$saldoPCC ?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">IVA CREDITO FISCAL</td>
-                          <th><?php echo "$".$saldoIVACF ?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">INVENTARIO FINAL</td>
-                          <th><?php echo "$".$inventariofinal ?></th>
-                        </tr>
-                        <tr>
-                          <th>TOTAL ACTIVO CORRIENTE</th>
-                          <th><?php echo "$".$saldoActivoCorriente=$saldoEYE+$saldoPCC+$saldoIVACF+$inventariofinal?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <th>ACTIVO NO CORRIENTE</th>
-                        </tr>
-                        <tr>
-                          <th>TOTAL ACTIVO NO CORRIENTE</th>
-                          <th><?php echo "$".$saldoActivoNC=0?></th>
-                        </tr>
-                        <tr>
-                          <th>TOTAL ACTIVO</th>
-                          <th><?php echo "$".$saldoTotalActivos=$saldoActivoCorriente+$saldoActivoNC?></th>
-                        </tr>
-                        <tr><th>.</th></tr>
-                        <tr><th>.</th></tr>
-                        <tr><th>.</th></tr>
-                        <tr class='bg-info'>
-                          <th>PASIVO</th>
-                        </tr>
-                        <tr class='bg-light'>
-                          <th>PASIVO CORRIENTE</th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">CUENTAS POR PAGAR COMERCIALES</td>
-                          <th><?php echo "$".$saldoCPP?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">IVA DEBITO FISCAL</td>
-                          <th><?php echo "$".$saldoIVADF?></th>
-                        </tr>
-                        <tr class='bg-light'>
-                          <th>TOTAL PASIVO CORRIENTE</th>
-                          <th><?php echo "$".$saldoPasivoC=$saldoCPP+$saldoIVADF?></th>
-                        </tr>
-                        <tr class="bg-light">
-                          <th>PASIVO NO CORRIENTE</th>
-                        </tr>
-                        <tr class='bg-light'>
-                          <th>TOTAL PASIVO NO CORRIENTE</th>
-                          <th><?php echo "$".$saldoPasivoNC=0?></th>
-                        </tr>
-                        <tr class="bg-info">
-                          <th>TOTAL PASIVO</th>
-                          <th><?php echo "$".$saldoTotalPasivos=$saldoPasivoC+$saldoPasivoNC?></th>
-                        </tr>
-                         <tr><th>.</th></tr>
-                        <tr><th>.</th></tr>
-                        <tr><th>.</th></tr>
-                        <tr class="bg-info">
-                          <th>PATRIMONIO</th>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">RESERVA LEGAL</td>
-                          <td align="center"></td>
-                        </tr>
-                        <tr class="bg-light">
-                          <td align="left">UTILIDAD DEL EJERCICIO</td>
-                          <td align="center"></td>
-                        </tr>
-                        <tr class="bg-info">
-                          <th align="center">TOTAL PATRIMONIO</td>
-                          <td align="center"></td>
-                        </tr>
-                        <tr class="bg-info">
-                          <th>TOTAL PASIVO + PATRIMONIO</th>
-                          <td align="center"></td>
-                        </tr>
-                      <?php
-                        $result = $conexion->query("select c.nombrecuenta as nombre,c.idcatalogo as id, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto FROM catalogo as c where SUBSTRING(c.codigocuenta,1,'2')='13' and c.nivel='3' ORDER BY c.codigocuenta ASC");
                         
-                        
-                        // PATRIMONO
-                        $result = $conexion->query("select c.nombrecuenta as nombre,c.idcatalogo as id, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto FROM catalogo as c where SUBSTRING(c.codigocuenta,1,'2')='31' and c.nivel='3' ORDER BY c.codigocuenta ASC");
-                        
+                        <?php
+                          //impresion de la seccion
+                          foreach($arregloSeccionPrincipal as $seccion){
+                            $saldoSeccion = 0;
+                            echo <<<END
+                            <tr class="bg-info">
+                            <td colspan='2' align='center'>$seccion[1]</td>
+                            </tr>
+                            END;
+
+                            //impresion del grupo
+                            foreach($arregloGrupo as $grupo){
+                              $saldoGrupo = 0;
+                              $sec = substr($grupo[0],0,1);
+                              if($sec==$seccion[0]){
+                                echo <<<END
+                                <tr class="bg-light">
+                                <td colspan='2' align='center'>$grupo[1]</td>
+                                </tr>
+                                END;
+
+                                //impresion de la cuenta de control
+                                foreach($tablaCuentas as $control){
+                                  $gru = substr($control[0],0,2);
+                                  if($gru == $grupo[0]){
+                                    if($control[2]!=0){
+                                      echo <<<END
+                                      <tr class="bg-light">
+                                      <td align="left">$control[1]</td>
+                                      <td align="center">$control[2]</td>
+                                      </tr>
+                                      END;
+                                      $saldoGrupo = $saldoGrupo + $control[2];
+                                    }
+                                  }else{
+                                    continue;
+                                  }
+                                }
+
+                                echo <<<END
+                                <tr class='bg-light'>
+                                <td align='center'>TOTAL $grupo[1]</td>
+                                <td align='center'>$saldoGrupo</td>
+                                </tr>
+                                END;
+                                $saldoSeccion = $saldoSeccion + $saldoGrupo;
+                              }else{
+                                continue;
+                              }
+                            }
+
+                            echo <<<END
+                            <tr class="bg-success">
+                            <td align="center">TOTAL $seccion[1]</td>
+                            <td align="center">$saldoSeccion</td>
+                            </tr>
+                            END;
+                          }
                         ?>
                       </tbody>
                     </table>
   
-</body>
-
 
 
 
